df5080de5f328e2d30214ea3db9a300f11eee5d6
ChaseTeague <teaguechase@yahoo.com>
2024-01-28 20:56:10 -0500

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -7,4 +7,9 @@ describe('pointsForWord', () => {
         const points = pointsForWord(word);
         expect(points).toBe(7);
     })
+    it('handles uppercase and lowercase input', () => {
+        const word = "tEsT";
+        const points = pointsForWord(word);
+        expect(points).toBe(7);
+    })
 })
